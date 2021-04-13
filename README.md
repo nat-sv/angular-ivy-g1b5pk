0da1e6ae9655153becd52dd5f7d7b85e3c38f4b7
nat-sv <201822031@tese.edu.mx>
2021-04-13 13:22:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-g1b5pk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-g1b5pk)
---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent implements OnInit {
+
+  constructor() { }
+  numero:number = 10;
+
+  ngOnInit():void {
+  }
+
+}
